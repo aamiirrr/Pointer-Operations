5fce67ed70058174a6baf84f9b21af1f4007a30c
Aamir Ayaan Quraishi <aamir.quraishi.btech2023@sitpune.edu.in>
2024-09-26 11:52:54 +0530

    Create call_by_value.cpp

---
+++ call_by_value.cpp
@@ -0,0 +1,28 @@
+//Aamir Ayaan Quraishi
+//23070123002
+//AIM - do swapping using pass by value.
+
+#include <iostream>
+using namespace std;
+
+void swap(int x, int y) {
+    int temp;
+    temp = x;
+    x = y;
+    y = temp;
+}
+
+int main() {
+    int a = 5, b = 10;
+    swap(a, b);
+    cout << "The value of a is: " << a << endl;
+    cout << "The value of b is: " << b << endl;
+    
+    return 0;
+}
+
+/*
+OUTPUT
+The value of a is: 5
+The value of b is: 10
+*/
